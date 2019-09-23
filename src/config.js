1ab61f8a901dca3bf98e62ba0418924bfe3ff060
John Chan <johnchancontact@gmail.com>
2019-09-23 15:43:24 -0400

    preparing for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  TOKEN_KEY: "91d30948-5750-4669-8263-43e465cb8e1a",
-  API_ENDPOINT: "http://localhost:8000/api"
+  TOKEN_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: "https://afternoon-wildwood-59829.herokuapp.com/"
 };
