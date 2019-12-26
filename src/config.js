39bb33c4906056f92c25316bd2d0daec0bb19f52
John Chan <johnchancontact@gmail.com>
2019-12-26 13:18:23 -0500

    added loading gif to login page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   TOKEN_KEY: process.env.REACT_APP_API_KEY,
-  // API_ENDPOINT: "https://afternoon-wildwood-59829.herokuapp.com/api"
-  API_ENDPOINT: "http://localhost:8000/api"
+  API_ENDPOINT: "https://afternoon-wildwood-59829.herokuapp.com/api"
+  // API_ENDPOINT: "http://localhost:8000/api"
 };
